e2be18f6945e2dfd67de0c0eb2a5abf943437d0f
APPLE <apple@MacBook-Pro-de-APPLE.local>
2022-03-14 18:06:20 -0500

    [feature] - add state in star game screen & set keyboard dismiss

--- src/screens/start-game-screen/index.js
+++ src/screens/start-game-screen/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Text,
     View,
     TextInput,
-    Button
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard,
 } from 'react-native';
 import { colors } from '../../constants/theme';
 import Header from '../../components/header';
@@ -12,34 +14,57 @@ import Input from '../../components/input';
 import { styles } from './styles';
 
 const StartGameScreen = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState('');
+    
     const handleOnChange = (text) => {
-        console.warn(text);
+        setInputValue(text.replace(/[^0-9]/g, ''));
     }
+
+    const handleResetInput = () => {
+        setInputValue('');
+        setConfirmed(false);
+    }
+
+    const handleConfirmInput = () => {
+        const chosenNumber = parseInt(inputValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setInputValue('');
+    }
+
     return (
-        <View style={styles.container}> 
-            <Header title='Adivina el número' />
-            <Card>
-                <Text style={styles.cardTitle}>Empezar Juego</Text>
-                <View style={styles.inputContainer}>
-                    <Text style={styles.label}>Elija un número</Text>
-                    <Input 
-                        blurOnSubmit
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        keyboardType='number-pad'
-                        placeholder='Ej: 11' 
-                        placeholderTextColor={colors.placeholderColor}
-                        maxLength={2}
-                        handleOnChange={(value) => handleOnChange(value)}
-                        returnKeyType='done'
-                    />
-                </View>
-                <View style={styles.buttonsContainer}>
-                    <Button title='Limpiar' onPress={() => null} color='#52528C'/>
-                    <Button title='Confirmar' onPress={() => null} color='#52528C'/>
-                </View>
-            </Card>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}> 
+                <Header title='Adivina el número' />
+                <Card>
+                    <Text style={styles.cardTitle}>Empezar Juego</Text>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Elija un número</Text>
+                        <Input 
+                            blurOnSubmit
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                            keyboardType='number-pad'
+                            placeholder='Ej: 11' 
+                            placeholderTextColor={colors.placeholderColor}
+                            maxLength={2}
+                            handleOnChange={(value) => handleOnChange(value)}
+                            returnKeyType='done'
+                            value={inputValue}
+                        />
+                    </View>
+                    <View style={styles.buttonsContainer}>
+                        <Button title='Limpiar' onPress={() => handleResetInput()} color='#52528C'/>
+                        <Button title='Confirmar' onPress={() => handleConfirmInput()} color='#52528C'/>
+                    </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
